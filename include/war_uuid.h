92104c49858c7c512896a61b9c00b29443c88bd9
Jarle (jgaa) Aase <jarle@jgaa.com>
2015-12-27 21:10:34 +0200

    Added uuid support required by warftpd

--- include/war_uuid.h
+++ include/war_uuid.h
@@ -12,4 +12,8 @@ inline std::string get_uuid_as_string() {
     return boost::uuids::to_string(uuid);
 }
 
+inline boost::uuids::uuid get_uuid_from_string(const std::string& str) {
+    return boost::uuids::string_generator()(str);
+}
+
 } // war
